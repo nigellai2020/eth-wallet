14f60037d6a9f1e8de1e180df8bb6f3570187e40
instajs <ycwong@instajs.com>
2021-10-19 15:13:39 +0800

    update stringToBytes32

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -3,7 +3,7 @@ export declare function sleep(millisecond: number): Promise<unknown>;
 export declare function numberToBytes32(value: number | BigNumber, prefix?: boolean): string;
 export declare function padLeft(string: string, chars: number, sign?: string): string;
 export declare function padRight(string: string, chars: number, sign?: string): string;
-export declare function stringToBytes32(value: string | string[], prefix?: boolean): string | string[];
+export declare function stringToBytes32(value: string | string[]): string | string[];
 export declare function addressToBytes32(value: string, prefix?: boolean): string;
 export declare function bytes32ToAddress(value: string): string;
 export declare function bytes32ToString(value: string): string;
