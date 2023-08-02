68473249550aa429ee4061d5b19d21727c31d6b2
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-08-02 17:08:26 +0800

    Update addAmdModuleName.bash

--- types/approvalModel/ERC20ApprovalModel.d.ts
+++ types/approvalModel/ERC20ApprovalModel.d.ts
@@ -1,3 +1,4 @@
+/// <amd-module name="@ijstech/eth-wallet/approvalModel/ERC20ApprovalModel.ts" />
 import { BigNumber } from 'bignumber.js';
 import { IRpcWallet } from "../wallet";
 import { ITokenObject } from '../types';
