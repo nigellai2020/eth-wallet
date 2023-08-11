1b28eab26ac74869dbee52451494ace8b51d568c
benho-ijs <benho@instajs.com>
2023-08-11 12:08:18 +0800

    add soliditySha3/toChecksumAddress to utils

--- types/nodeUtils.d.ts
+++ types/nodeUtils.d.ts
@@ -26,6 +26,8 @@ export declare function fromDecimals(value: BigNumber | number | string, decimal
 export declare function toString(value: any): any;
 export declare const nullAddress = "0x0000000000000000000000000000000000000000";
 export declare function constructTypedMessageData(domain: IEIP712Domain, customTypes: EIP712TypeMap, primaryType: string, message: Record<string, unknown>): TypedMessage<MessageTypes>;
+export declare function soliditySha3(...val: any[]): any;
+export declare function toChecksumAddress(address: string): any;
 export declare function getSha3HashBufferFunc(wallet: Wallet, abi: IMerkleTreeAbiItem[]): (leafData: Record<string, any>) => string;
 export declare function generateMerkleTree(wallet: Wallet, options: IMerkleTreeOptions): MerkleTree;
 export declare function getMerkleProofs(wallet: Wallet, tree: MerkleTree, options: IGetMerkleProofOptions): string[][];
